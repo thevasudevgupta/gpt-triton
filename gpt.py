5b2a1efcfc88a2872469eea7fc7ad4b41f729fa9
Vasudev Gupta <7vasudevgupta@gmail.com>
2024-08-23 04:58:08 +0000

    not perfect match but close

--- gpt.py
+++ gpt.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 
 import torch
 import torch.nn as nn
+from tqdm.auto import tqdm
 from transformers import AutoTokenizer
 from transformers import GPT2Model as HFGPT2
 
@@ -24,7 +25,11 @@ class FusedAttention(nn.Module):
         self.c_attn_bias = nn.Parameter(torch.rand((3 * hidden_size,)))
 
         self.c_proj_weight = nn.Parameter(torch.rand(hidden_size, hidden_size))
-        self.c_proj_bias = nn.Parameter(torch.rand(hidden_size, hidden_size))
+        self.c_proj_bias = nn.Parameter(
+            torch.rand(
+                hidden_size,
+            )
+        )
 
     def forward(self, x):
         residual = x
@@ -93,11 +98,10 @@ class FusedMLP(nn.Module):
 @dataclass
 class GPTConfig:
     vocab_size: int = 50304
-    block_size: int = 1024
-
+    block_size: int = 512
     n_layer: int = 12
-    n_head: int = 2
-    n_embd: int = 256
+    n_head: int = 12
+    n_embd: int = 768
     dropout: float = 0.1
 
 
@@ -106,18 +110,20 @@ class FusedGPT(nn.Module):
         super().__init__()
         self.config = config
 
-        self.wte = nn.Embedding(config.vocab_size, config.n_embd)
-        self.wpe = nn.Embedding(config.block_size, config.n_embd)
+        self.wte_weight = nn.Parameter(torch.rand(config.vocab_size, config.n_embd))
+        self.wpe_weight = nn.Parameter(torch.rand(config.block_size, config.n_embd))
 
-        self.blocks = [
-            nn.Sequential(
-                FusedAttention(
-                    config.n_embd, config.n_head, dropout_prob=config.dropout
-                ),
-                FusedMLP(config.n_embd, dropout_prob=config.dropout),
-            )
-            for _ in range(config.n_layer)
-        ]
+        self.blocks = nn.ModuleList(
+            [
+                nn.Sequential(
+                    FusedAttention(
+                        config.n_embd, config.n_head, dropout_prob=config.dropout
+                    ),
+                    FusedMLP(config.n_embd, dropout_prob=config.dropout),
+                )
+                for _ in range(config.n_layer)
+            ]
+        )
         self.layer_norm_weight = nn.Parameter(torch.ones((config.n_embd,)))
         self.layer_norm_bias = nn.Parameter(torch.zeros((config.n_embd,)))
 
@@ -129,7 +135,7 @@ class FusedGPT(nn.Module):
         # it does causal automatically, no need of separate attention/padding mask
         dropout_prob = self.config.dropout_prob if self.training else 0.0
         x = fused_embeddings(
-            x, self.wte.weight.data, self.wpe.weight.data, dropout_prob=dropout_prob
+            x, self.wte_weight.data, self.wpe_weight.data, dropout_prob=dropout_prob
         )
         for block in self.blocks:
             x = block(x)
@@ -145,19 +151,56 @@ class FusedGPT(nn.Module):
         return x
 
 
-config = GPTConfig()
-model = FusedGPT(config)
-
-
-def convert_huggingface_to_triton(state_dict, config):
-    return state_dict, config
+def convert_huggingface_to_triton(hf_sd, hf_config):
+    config = GPTConfig(
+        vocab_size=hf_config.vocab_size,
+        block_size=hf_config.n_ctx,
+        n_layer=hf_config.n_layer,
+        n_head=hf_config.n_head,
+        n_embd=hf_config.n_embd,
+        dropout=0.1,
+    )
+    mapping = {
+        "wte.weight": "wte_weight",
+        "wpe.weight": "wpe_weight",
+        "ln_f.weight": "layer_norm_weight",
+        "ln_f.bias": "layer_norm_bias",
+    }
+    block = {
+        "h.{i}.ln_1.weight": "blocks.{i}.0.layer_norm_weight",
+        "h.{i}.ln_1.bias": "blocks.{i}.0.layer_norm_bias",
+        "h.{i}.attn.bias": None,
+        "h.{i}.attn.c_attn.weight": "blocks.{i}.0.c_attn_weight",
+        "h.{i}.attn.c_attn.bias": "blocks.{i}.0.c_attn_bias",
+        "h.{i}.attn.c_proj.weight": "blocks.{i}.0.c_proj_weight",
+        "h.{i}.attn.c_proj.bias": "blocks.{i}.0.c_proj_bias",
+        "h.{i}.ln_2.weight": "blocks.{i}.1.layer_norm_weight",
+        "h.{i}.ln_2.bias": "blocks.{i}.1.layer_norm_bias",
+        "h.{i}.mlp.c_fc.weight": "blocks.{i}.1.ffn1_weight",
+        "h.{i}.mlp.c_fc.bias": "blocks.{i}.1.ffn1_bias",
+        "h.{i}.mlp.c_proj.weight": "blocks.{i}.1.ffn2_weight",
+        "h.{i}.mlp.c_proj.bias": "blocks.{i}.1.ffn2_bias",
+    }
+    for k, v in block.items():
+        if v is None:
+            continue
+        for i in range(config.n_layer):
+            mapping[k.format(i=i)] = v.format(i=i)
+    sd = {}
+    for k, v in tqdm(hf_sd.items()):
+        k = mapping[k]
+        sd[k] = v
+    return sd, config
 
 
 model_id = "gpt2"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 hf_model = HFGPT2.from_pretrained(model_id).eval()
-# state_dict, config = convert_huggingface_to_triton(hf_model.state_dict(), hf_model.config)
+state_dict, config = convert_huggingface_to_triton(
+    hf_model.state_dict(), hf_model.config
+)
 model = FusedGPT(config).eval()
+model.load_state_dict(state_dict)
 
 with torch.no_grad():
     string = "I am vasudev gupta. I like AI."
@@ -165,7 +208,8 @@ with torch.no_grad():
     print(inputs)
     hf_out = hf_model(**inputs).last_hidden_state
     out = model(inputs["input_ids"])
+    print((out - hf_out).abs().max())
+
     import ipdb
 
     ipdb.set_trace()
-    print((out - hf_out).abs().max())
